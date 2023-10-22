d314b5e6d8045d5694567222b204e28db53acefd
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-10-22 17:47:49 +0200

    add serde dependencies and read configuration file
    adds the necessary dependencies for the [serde](https://serde.rs/) library to `cargo.lock` and `cargo.toml` and also adds a new configuration file, `margareth.json`, to read the configuration for the application. additionally, the `main.rs` has been updated to include the logic to read the configuration from the `margareth.json` file.
    
    - added serde and serde_json crates to `cargo.lock` and `cargo.toml`
    - added `margareth.json` for configuration
    - updated `main.rs` to read the configuration from `margareth.json`

--- src/main.rs
+++ src/main.rs
@@ -19,5 +19,19 @@ pub struct Config {
 
 fn main() {
   let args = Cli::parse();
-  println!("{:?}", args);
+  match args.config {
+    Some(config) => match config {
+      ConfigView::Config(config) => {
+        dbg!(config);
+        let file = get_config();
+        dbg!(file);
+      }
+    },
+    _ => println!("No config"),
+  }
+}
+
+fn get_config() -> serde_json::Value {
+  let file = std::fs::read_to_string("margareth.json").unwrap();
+  file.parse::<serde_json::Value>().unwrap()
 }
