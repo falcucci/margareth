148776eaf5cfcf2e2afdfc47897863b168da2b46
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-10-22 22:14:52 +0200

    add box type to app arguments
    adds a box type to the app arguments in the margareth enum in `src/main.rs`. the `warn` attribute was also added to the enum to suppress a clippy warning.
    
    the `app` variant was updated to accept a `box<app::args>` instead of `app::args`. the `run` function was also updated to dereference `app` when calling `app::run`.

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,17 @@ mod config;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
+#[warn(clippy::large_enum_variant)]
 enum Margareth {
   Config(config::Args),
-  App(app::Args),
+  App(Box<app::Args>),
 }
 
 fn main() {
   let margareth = Margareth::parse();
   let result = match margareth {
     Margareth::Config(config) => config::run(config),
-    Margareth::App(app) => app::run(app),
+    Margareth::App(app) => app::run(*app),
   };
 
   if let Err(err) = result {
