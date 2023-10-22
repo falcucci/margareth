7963d03ff53f47add3720002f78095ce1e002fd7
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-10-22 22:11:51 +0200

    adda --app command line options
    adds command line options to the `src/app.rs` file. the options added include `create`, `list`, `delete`, `info`, `start`, `stop`, `env_set`, `env_unset`, `secret_set`, `secret_unset`, `logs`, `auto_scale`, `delete_pods`, `set_vhost` and `unset_vhost`.
    
    ```diff
    diff --git a/src/app.rs b/src/app.rs
    index 47becb1d7619..622b5f4bc690 100644
    --- a/src/app.rs
    +++ b/src/app.rs
    @@ -3,7 +3,21 @@ use crate::config::get_config;
     #[derive(clap::args, debug)]
     #[clap(author, version, about, long_about = none)]
     pub struct args {
    -  pub name: string,
    +  create: option<string>,
    +  list: option<string>,
    +  delete: option<string>,
    +  info: option<string>,
    +  start: option<string>,
    +  stop: option<string>,
    +  env_set: option<string>,
    +  env_unset: option<string>,
    +  secret_set: option<string>,
    +  secret_unset: option<string>,
    +  logs: option<string>,
    +  auto_scale: option<string>,
    +  delete_pods: option<string>,
    +  set_vhost: option<string>,
    +  unset_vhost: option<string>,
     }
    ```
    
    this commit adds command line options to the `src/app.rs` file. the options added include `create`, `list`, `delete`, `info`, `start`, `stop`, `env_set`, `env_unset`, `secret_set`, `secret_unset`, `logs`, `auto_scale`, `delete_pods`, `set_vhost` and `unset_vhost`.

--- src/app.rs
+++ src/app.rs
@@ -3,7 +3,21 @@ use crate::config::get_config;
 #[derive(clap::Args, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
-  pub name: String,
+  create: Option<String>,
+  list: Option<String>,
+  delete: Option<String>,
+  info: Option<String>,
+  start: Option<String>,
+  stop: Option<String>,
+  env_set: Option<String>,
+  env_unset: Option<String>,
+  secret_set: Option<String>,
+  secret_unset: Option<String>,
+  logs: Option<String>,
+  auto_scale: Option<String>,
+  delete_pods: Option<String>,
+  set_vhost: Option<String>,
+  unset_vhost: Option<String>,
 }
 
 pub fn run(_args: Args) -> Result<(), Box<dyn std::error::Error>> {
